6fff7c5533a0505ae52104527810c37b87085bb6
Maisson Davis <68596011+mtdavis725@users.noreply.github.com>
2020-12-17 05:21:09 -0500

    Upload weather-app repository
    React app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+const api = {
+  key: "b3401117034a7fa9fd5f263b9b530349",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       Weather
     </div>
   );
 }
